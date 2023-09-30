5dda921cb99cbceace524b35b0a0c00e8dad5041
layden diaz <layden@student.dev-island.org>
2023-09-30 12:23:35 -1000

    created book class/component module then added tailwindCss to project

---
+++ book.js
@@ -0,0 +1,44 @@
+const LIBRARY = document.querySelector(".library");
+
+class Book extends HTMLElement {
+  constructor(author, title, numberOfPages) {
+    super();
+
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+
+    const AUTHOR = document.createElement("h2");
+    const TITLE = document.createElement("h1");
+    const NUMBER_OF_PAGES = document.createElement("p");
+
+    this.innerHTML = "CUSTOMMMMM";
+  }
+
+  displayBooks(MY_LIBRARY) {
+    MY_LIBRARY.forEach((currentBook) => {
+      const BOOK = document.createElement("div");
+      const AUTHOR = document.createElement("h2");
+      const TITLE = document.createElement("h1");
+      const NUMBER_OF_PAGES = document.createElement("p");
+      console.log(currentBook);
+      console.log(currentBook.title);
+
+      AUTHOR.textContent = currentBook.author;
+      TITLE.textContent = currentBook.title;
+      NUMBER_OF_PAGES.textContent = currentBook.numberOfPages;
+
+      AUTHOR.classList.add("text-red-800");
+      BOOK.className = "bg-gray-300 mb-9 w-1/4 text-center";
+
+      BOOK.appendChild(AUTHOR);
+      BOOK.appendChild(TITLE);
+      BOOK.appendChild(NUMBER_OF_PAGES);
+      LIBRARY.appendChild(BOOK);
+    });
+  }
+}
+
+customElements.define("book-item", Book);
+
+export default Book;
--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const myLibrary = [];
+import Book from "./book.js";
 
-function Book(author, title, numberOfPages) {
-  this.author;
-  this.title;
-  this.numberOfPages;
-}
+const LIBRARY = document.querySelector(".library");
+
+const MY_LIBRARY = [];
+
+// function Book(author, title, numberOfPages) {
+//   this.author = author;
+//   this.title = title;
+//   this.numberOfPages = numberOfPages;
+// }
 
 function addBookToLibrary() {
-  // do stuff here
-  const book = new Book("blue", "hello", 3);
-  myLibrary.push(book);
+  const book = new Book("LAYDEN", "ANYTHING", 3);
+  const book2 = new Book("you", "ANYTHING", 3);
+  const book3 = new Book("me", "ANYTHING", 3);
+  MY_LIBRARY.push(book, book2, book3);
+  console.log(MY_LIBRARY);
+
+  book.displayBooks(MY_LIBRARY);
 }
 
+// function displayBooks() {
+//   LIBRARY.innerHTML = "";
+
+// }
+
 addBookToLibrary();
+// displayBooks();
